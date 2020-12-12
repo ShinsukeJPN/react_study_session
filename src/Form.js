73d19c73e2319d5926a2cc64d4cf9256671a3022
ShinsukeNakamura <pgcxw5115@yahoo.co.jp>
2020-12-12 23:21:51 +0900

    fix favorite, delete

--- src/Form.js
+++ src/Form.js
@@ -3,9 +3,6 @@ import SubmitButton from './SubmitButton';
 import favImage from './images/favorited.png';
 import unFavImage from './images/unfavorited.png';
 
-const favSrcPath = './images/favorited.png';
-const unFavSrcPath = './images/unfavorited.png';
-
 class Form extends React.Component {
   form = {
     marginLeft: "auto",
@@ -21,7 +18,8 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      text: ''
     };
 
     this.onChange = this.onChange.bind(this);
@@ -31,10 +29,14 @@ class Form extends React.Component {
     this.favoriteItem = this.favoriteItem.bind(this);
   }
 
+  // Assigns text state
   onChange(e){
-    this.input = e.target.value;
+    this.setState((state)=>({
+      text: e.target.value
+    }));
   }
 
+  // Tweet
   onSubmit(e){
     let d = new Date(),
         second = '' + d.getSeconds(),
@@ -45,55 +47,66 @@ class Form extends React.Component {
         year = d.getFullYear();
     let formatted_date = [year, month, day].join('-') + ' ' + hour + ':' + min + ':' + second;
 
+    const tweetList = this.state.data.concat({tweet: this.state.text, timeStamp: formatted_date, isFavorited: false});
+
     this.setState((state)=>({
-      data: state.data.concat({tweet: this.input, timeStamp: formatted_date, isFavorited: false}).reverse()
-      
+      data: tweetList,
+      text: ''
     }));
 
     e.preventDefault();
   }
 
+  // Delete
   deleteItem(e){
     let id = e.currentTarget.getAttribute('data-id');
-    let deleted_data = this.state.data.splice(id, 1)
+    this.state.data.splice(id, 1)
     this.setState((state)=>({
       data: state.data.reverse()
     }));
   }
 
-  copyClipBoardItem(e){
-
-  }
-
+  // Favorite
   favoriteItem(e){
     let id = e.currentTarget.getAttribute('data-id');
     const item = this.state.data.slice();
-    item[id].isFavorited = true
+    item[id].isFavorited = !item[id].isFavorited
     this.setState((state)=>({
       data: item
     }));
+  }
+
+  // Copy on clipboard
+  copyClipBoardItem(e) {
+    let text = e.currentTarget.getAttribute('data-text');
+    if(navigator.clipboard){
+        navigator.clipboard.writeText(text);
+        return true;
+    } else {
+        return false;
+    }
   }
 
   render() {
     return (
       <div style={this.form}>
         <form onSubmit={this.onSubmit}>
-          <textarea onChange={this.onChange} required="true" maxLength="140" />
+          <textarea onChange={this.onChange} required="true" maxLength="140" value={this.state.text}/>
           <SubmitButton value="ツイート"/>
         </form>
 
         {this.state.data.map((value, i)=>(
          <li key={i}>
-           {value.tweet} / {value.timeStamp}
+           {value.tweet} / {value.timeStamp}　{i}
            <button onClick={this.deleteItem} data-id={i}>削除</button>
            <button onClick={this.copyClipBoardItem} data-text={value.tweet}>クリップボードにコピー</button>
-           {this.isFavorited ?
-              <img src={favSrcPath} data-id={i} style={this.fav} onClick={this.favoriteItem}/>
+           {value.isFavorited ?
+              <img src={favImage} data-id={i} style={this.fav} onClick={this.favoriteItem}/>
             :
-              <img src={unFavSrcPath} data-id={i} style={this.fav} onClick={this.favoriteItem}/>
+              <img src={unFavImage} data-id={i} style={this.fav} onClick={this.favoriteItem}/>
            }
          </li>
-         ))}
+         )).reverse()}
       </div>
     );
   }
